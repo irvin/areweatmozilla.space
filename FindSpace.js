3013299980b8af7b3ec024384ec2e92ed7f00425
Irvin Chen <irvinfly@gmail.com>
2026-03-13 19:25:16 +0800

    update space loc

--- FindSpace.js
+++ FindSpace.js
@@ -2,103 +2,42 @@ var mozSpaces = {
   dat: [
     {
       name: "Community Space Taipei",
-      addr_en: "3rd Fl., No. 94, Sec. 1, Ba-de Rd., Zhongzheng District, Taipei City 100, Taiwan",
-      addr: "100台灣台北市中正區八德路一段94號3F",
-      lat: 25.044113,
-      long: 121.532295
-    },
-    {
-      name: "Community Space Bangalore",
-      addr_en: "No 10, 3rd Floor, Serenity, 'A' Cross, Austin Town, Bangalore, Pin 560 047, India",
-      lat: 12.963291,
-      long: 77.6142979
-    },
-    {
-      name: "Community Space Manila",
-      addr_en: "Molave Building, 2231 Chino Roces Ave, Makati, Metro Manila, Philippines",
-      lat: 14.5468499,
-      long: 121.0159104
+      addr_en: "Rm. 1105, 11F, 99 Chongqing S. Rd. Sec I, Zhongzheng Dist., Taipei City 100, Taiwan",
+      addr: "100臺北市中正區重慶南路一段99號1105室世界大樓11樓",
+      lat: 25.0429756,
+      long: 121.5135698
     },
     {
       name: "Community Space Jakarta",
-      addr_en: "Daerah Khusus Ibukota Jakarta, Jl. Cikini Raya No.9, Cikini, Menteng, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta, Indonesia",
-      lat: -6.1876205,
-      long: 106.8345783
-    },
-    {
-      name: "Mountain View",
-      addr_en: "331 E. Evelyn Avenue, Mountain View, CA 94041, USA",
-      lat: 37.3872783,
-      long: -122.0602164
-    },
-    {
-      name: "Auckland",
-      addr_en: "Level 7, 5 Short St., Newmarket, Auckland 1023, New Zealand",
-      lat: -36.8664438,
-      long: 174.7771253
-    },
-    {
-      name: "Beijing (Mozilla Online Ltd.)",
-      addr_en: "China Resources Building, Suite 1708, 8 Jianguomenbei Avenue, Dongcheng District, Beijing 100005, China",
-      addr: "北京市 东城区 建国门北大街8号 华润大厦 17层1708单元",
-      lat: 39.9129981,
-      long: 116.4318727
+      addr_en: "SOHO Pancoran, Tower Noble lantai 11, unit N-1109, RT.1/RW.5, Tebet Bar., Kec. Tebet, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12810, Indonesia",
+      lat: -6.2424803,
+      long: 106.845506
     },
     {
       name: "Berlin",
-      addr_en: "Haus 10, Treppe 6, Voltastr. 5, 13355 Berlin, Germany",
-      lat: 52.54216,
-      long: 13.39031
-    },
-    {
-      name: "London",
-      addr_en: "Metal Box Factory, Suite 441, 4th floor, 30 Great Guildford Street, London SE1 0HS, UK",
-      lat: 51.504629,
-      long: -0.097066
+      addr_en: "GSG-Hof, Schlesische Str. 27/Gebäude 3, 4. Obergeschoss, 10997 Berlin, Germany",
+      lat: 52.4994197,
+      long: 13.4489385
     },
     {
       name: "Paris",
-      addr_en: "16 Bis Boulevard Montmartre, Paris 75009, France",
-      lat: 48.8721388,
-      long: 2.3411542
+      addr_en: "34 Rue Laffitte, 75009 Paris, France",
+      lat: 48.8735717,
+      long: 2.3379989
     },
-    {
-      name: "Portland",
-      addr_en: "Brewery Block 2, 1120 NW Couch St, Suite 320, Portland, OR 97209, USA",
-      lat: 45.5236849,
-      long: -122.6827884
-    },
+    /*
     {
       name: "San Francisco",
-      addr_en: "2 Harrison Street, Suite 175, San Francisco, CA 94105, USA",
+      addr_en: "149 New Montgomery Street, 4th floor, San Francisco, CA 94105, USA",      
       lat: 37.7895035,
       long: -122.3890646
     },
-    {
-      name: "Taipei",
-      addr_en: "4F-A1, No. 106, Sec. 5, Xinyi Rd, Xinyi Dist., Taipei City 11047, Taiwan",
-      addr: "110台灣台北市信義區信義路五段106號4樓A1",
-      lat: 25.032329,
-      long: 121.567422
-    },
-    {
-      name: "Tokyo",
-      addr_en: "7-5-6 Roppongi, Minato-ku, Tokyo 106-0032, Japan",
-      addr: "東京都港区六本木7丁目5-6",
-      lat: 35.6651776,
-      long: 139.7277261
-    },
+    */
     {
       name: "Toronto",
       addr_en: "366 Adelaide St W, Suite 500, Toronto, ON M5V 1R9, Canada",
       lat: 43.6471131,
       long: -79.3942263
-    },
-    {
-      name: "Vancouver",
-      addr_en: "163 W Hastings St, Suite 209, Vancouver, BC V6B 1H5, Canada",
-      lat: 49.2824658,
-      long: -123.1091908
     }
   ],
   calcDistance: function(coord){
